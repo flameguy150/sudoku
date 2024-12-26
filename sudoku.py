11ba1a75b46a47d00deaec1100deda6c591f4d44
Justin Lee <flameguy150@gmail.com>
2024-12-26 01:22:30 -0600

    fixed highlight func, added borders, displaying numbers on grid squares

--- sudoku.py
+++ sudoku.py
@@ -21,7 +21,7 @@ grid_rectangles = []
 
 hovered = False
 		
-
+pygame.font.init()
 
 def mouse_print():
 	if event.type == pygame.MOUSEBUTTONDOWN:
@@ -53,9 +53,10 @@ def highlight():
 		if event.type == pygame.MOUSEMOTION:
 				if rectangle[0].collidepoint(pygame.mouse.get_pos()) and not hovered:
 					print("Mouse clicked on object!")
+					outline = draw_rectangle((0,0,0), rectangle[1][0] - 1, rectangle[1][1] - 1, rectangle[1][2] + 2, rectangle[1][3] + 2)
 					highlight_lol = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(rectangle[1]))
 					hovered = True
-				elif not rectangle[0].collidepoint(pygame.mouse.get_pos()) and hovered:
+				elif not rectangle[0].collidepoint(pygame.mouse.get_pos()): # draw white rectangle to replace red when cursor not over rectangle
 					# highlight_lol = pygame.draw.rect(screen, color, pygame.Rect(rectangle[1]))
 					draw_rectangle(color, rectangle[1][0], rectangle[1][1], rectangle[1][2], rectangle[1][3])
 					hovered = False
@@ -94,8 +95,29 @@ def draw_rectangle(the_color, left, top, width, height):
 	rect_info = (rect, (left, top, width, height))
 	# grid_rectangles.append(rect_info)
 
+def draw_line(the_color, x1, y1, x2, y2, width): # need this to draw thicker lines between the 9 3x3 boxes
+		line = pygame.draw.line(screen, the_color, (x1,y1), (x2,y2), width)
+
+def create_grid_outlines():
+	for i in range(4): #vertical lines
+		line = draw_line((0,0,0), 40 + 240*i, 75, 40 + 240*i, 525, 5)
+	for x in range(4): #horizontal lines
+		line = draw_line((0,0,0), 40, 75 + 150*x, 760, 75+ 150*x, 5)
+
+#
+
+font = pygame.font.Font(None,36)
+
+
+def numbers():#trying to see if i cant print numbers inside the boxes
+	inc = 1
+	for rectangle in grid_rectangles:
+		text_surface = font.render(str(inc), True, (0,0,255))
+		screen.blit(text_surface, rectangle[0])
+		inc += 1
 
 create_grid()
+create_grid_outlines()
 # Update the display using flip 
 pygame.display.flip() 
 
@@ -112,10 +134,20 @@ while running:
 		if event.type == pygame.QUIT: 
 			running = False
 		position = pygame.mouse.get_pos() 
+		
+		
 
 		mouse_print()
 		highlight()
+		create_grid_outlines()	
+		numbers()
+
 		pygame.display.flip() 
 		
 			   
 
+
+
+# In order to put anything on github, you want the following pattern
+# git init once
+# add ., commit, push
